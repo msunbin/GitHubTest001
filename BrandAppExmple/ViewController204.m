64851a0f7c5d027723676368073320c66e6a7fa0
Jaewoong Chang <jangpaul45@Jaewoong-ui-Mac-mini.local>
2014-03-09 11:25:53 +0900

    888

---
+++ BrandAppExmple/ViewController204.m
@@ -0,0 +1,38 @@
+//
+//  ViewController204.m
+//  BrandAppExmple
+//
+//  Created by Jaewoong Chang on 2014. 3. 9..
+//  Copyright (c) 2014년 compudent. All rights reserved.
+//
+
+#import "ViewController204.h"
+
+@interface ViewController204 ()
+
+@end
+
+@implementation ViewController204
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
