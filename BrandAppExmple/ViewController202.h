764eaa3996a5100cb52e9ccbede173199421574f
Jaewoong Chang <jangpaul45@Jaewoong-ui-Mac-mini.local>
2014-03-09 11:15:31 +0900

    555

---
+++ BrandAppExmple/ViewController202.h
@@ -0,0 +1,13 @@
+//
+//  ViewController202.h
+//  BrandAppExmple
+//
+//  Created by Jaewoong Chang on 2014. 3. 9..
+//  Copyright (c) 2014년 compudent. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController202 : UIViewController
+
+@end
---
+++ ViewController201.m
@@ -0,0 +1,38 @@
+//
+//  ViewController201.m
+//  BrandAppExmple
+//
+//  Created by Jaewoong Chang on 2014. 3. 9..
+//  Copyright (c) 2014년 compudent. All rights reserved.
+//
+
+#import "ViewController201.h"
+
+@interface ViewController201 ()
+
+@end
+
+@implementation ViewController201
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
